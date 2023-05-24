d0bd6731b9a4f27f4dd5a93b3712d4c0f1806dc2
Ema <emanuele.papale@gmail.com>
2023-05-24 12:00:52 +0200

    Project page images

--- js/master.js
+++ js/master.js
@@ -1,21 +1,18 @@
 // ********************************
-// Barba.js basic transiion
+// Barba.js 
 // ********************************
-// barba.init({
-//   transitions: [{
-//     name: 'opacity-transition',
-//     leave(data) {
-//       return gsap.to(data.current.container, {
-//         opacity: 0
-//       });
-//     },
-//     enter(data) {
-//       return gsap.from(data.next.container, {
-//         opacity: 0
-//       });
-//     }
-//   }]
-// });
+// tell Barba to use the css plugin
+barba.use(barbaCss);
+// init Barba
+barba.init({
+    transitions: [
+        {
+            name: 'fade',
+            leave() {},
+            enter() {},
+        }
+    ]
+});
 
 
 // ********************************
